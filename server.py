ff4515b4c3560b4c760972dbf04daa057b5b62c4
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-11-20 23:06:37 -0300

    add CORS support

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 import markdown
 
 from retirar import Retirar
 from productos import Producto, Productos
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 @app.route("/")
